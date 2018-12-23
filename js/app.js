0f032527e6c269387fafe413379122727ef6c63d
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-12-23 16:18:46 +0000

    change type writer to one line

---
+++ js/app.js
@@ -0,0 +1,2 @@
+// const myDiv = document.getElementsByClassName('nav');
+console.log('Hi! Thank you for checking my portfolio. If you would like to get in touch, please contact me at dorisfuying.huang@gmail.com');
