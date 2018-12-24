4ed5eb1de29ea2f147ba7bcc7b2c2dd013c9e327
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-12-24 00:39:12 +0000

    added curtain menu

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 // const myDiv = document.getElementsByClassName('nav');
-console.log('Hi! Thank you for checking my portfolio. If you would like to get in touch, please contact me at dorisfuying.huang@gmail.com');
+// console.log('Hi! Thank you for checking my portfolio. If you would like to get in touch, please contact me at dorisfuying.huang@gmail.com');
+/* Open when someone clicks on the span element */
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
