6714b8b56ae59f7147448db3cc4af6e2f19e8c43
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-12-28 01:25:43 +0800

    added project hover to change opacity

--- js/app.js
+++ js/app.js
@@ -46,3 +46,29 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = 'block';
   // dots[slideIndex-1].className += ' active';
 }
+
+
+
+
+document.getElementById('content-four').addEventListener('mouseover', mouseOver);
+document.getElementById('content-four').addEventListener('mouseout', mouseOut);
+document.getElementById('content-three').addEventListener('mouseover', mouseOver);
+document.getElementById('content-three').addEventListener('mouseout', mouseOut);
+document.getElementById('content-two').addEventListener('mouseover', mouseOver);
+document.getElementById('content-two').addEventListener('mouseout', mouseOut);
+document.getElementById('content-one').addEventListener('mouseover', mouseOver);
+document.getElementById('content-one').addEventListener('mouseout', mouseOut);
+
+function mouseOver() {
+  document.getElementById('content-four').style.opacity = '0.8';
+  document.getElementById('content-three').style.opacity = '0.8';
+  document.getElementById('content-two').style.opacity = '0.8';
+  document.getElementById('content-one').style.opacity = '0.8';
+}
+
+function mouseOut() {
+  document.getElementById('content-four').style.opacity = '0';
+  document.getElementById('content-three').style.opacity = '0';
+  document.getElementById('content-two').style.opacity = '0';
+  document.getElementById('content-one').style.opacity = '0';
+}
