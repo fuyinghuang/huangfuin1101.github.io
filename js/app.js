72bd35cd58c8dbc0f0ae5df9c41c52694097e86b
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-12-29 02:34:52 +0800

    added skills in about page

--- js/app.js
+++ js/app.js
@@ -72,3 +72,33 @@ function mouseOut() {
   document.getElementById('content-two').style.opacity = '0';
   document.getElementById('content-one').style.opacity = '0';
 }
+
+
+
+
+//
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+//
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
+
+
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
